9276f2b1d64b75590d40ccea5a519411072583a2
Joachim Løvgaard <joachim@loevgaard.dk>
2018-03-07 14:37:05 +0100

    fixed bug in pre remove listener

--- EventListener/OrderLineSubscriber.php
+++ EventListener/OrderLineSubscriber.php
@@ -8,6 +8,7 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
 use Loevgaard\DandomainFoundation\Entity\Generated\OrderLineInterface;
+use Loevgaard\DandomainStock\Entity\StockMovement;
 use Loevgaard\DandomainStock\Exception\CurrencyMismatchException;
 use Loevgaard\DandomainStock\Exception\UnsetCurrencyException;
 
@@ -43,6 +44,7 @@ class OrderLineSubscriber implements EventSubscriber
         if ($effectiveStockMovement && $effectiveStockMovement->getQuantity() !== 0) {
             $stockMovement = $effectiveStockMovement->inverse();
             $stockMovement
+                ->setType(StockMovement::TYPE_REGULATION) // we set the type as regulation since it is not a sale now
                 ->setOrderLineRemoved(true)
                 ->setOrderLine(null);
 
